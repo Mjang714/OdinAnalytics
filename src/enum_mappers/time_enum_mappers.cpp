8d2a6bbe753739aa1bca682a90bce3ef5903bcf3
Derek Huang <djh458@stern.nyu.edu>
2023-06-17 21:47:30 -0400

    disable warning about toupper narrowing int to char + add MSVC warning macros
    src/enum_mappers/time_enum_mappers.cpp
        Disable C4242, C4244 when making string uppercase
    
    src/helpers/warnings.h
        Add MSVC warning macros

--- src/enum_mappers/time_enum_mappers.cpp
+++ src/enum_mappers/time_enum_mappers.cpp
@@ -1,12 +1,17 @@
 #include "time_enum_mappers.h"
 
+#include "helpers/warnings.h"
 
 namespace oa::enum_mappers
 {
 	oa::time::DayCountRule MapInputToDayCountEnum(const std::string& input_str)
 	{
 		std::string key_str = input_str;
+		// disable C4242, C4244 warnings about int being narrowed to char
+		OA_MSVC_WARNING_PUSH()
+		OA_MSVC_WARNING_DISABLE(4242 4244)
 		std::ranges::transform(input_str.begin(), input_str.end(), key_str.begin(), ::toupper);
+		OA_MSVC_WARNING_POP()
 
 		if (TimeEnumMap().day_count_mappers.contains(key_str))
 		{
@@ -17,7 +22,7 @@ namespace oa::enum_mappers
 		{
 			throw std::invalid_argument(
 				std::format("{}:{}",
-				"Not a Valid day count convention please check input of: "+  input_str, 
+				"Not a Valid day count convention please check input of: "+  input_str,
 					"time_enum_mappers.cpp line 19 MapInputToDayCountEnum()"));
 		}
 
