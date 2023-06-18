d46ddeedc31576628408d1fbc29d737eb6bc58cd
Derek Huang <djh458@stern.nyu.edu>
2023-06-18 00:29:57 -0400

    allow use of system libraries for Linux + disable C4265 for unit tests
    cmake/FindDeps.cmake
        Only require use of environment variables when system libs aren't found
    
    src/unit_test/CMakeLists.txt
        Disable C4265 for MSVC which is unaddressable

--- cmake/FindDeps.cmake
+++ cmake/FindDeps.cmake
@@ -15,40 +15,58 @@ endif()
 # otherwise, add to include directories (SYSTEM ignores warnings)
 include_directories(SYSTEM ${ODIN_MAGIC_ENUM_ROOT})
 
-# find Boost. can set ODIN_BOOST_ROOT in environment or define
-if(DEFINED ENV{ODIN_BOOST_ROOT})
-    message(STATUS "Using ODIN_BOOST_ROOT $ENV{ODIN_BOOST_ROOT} from env")
-    set(ODIN_BOOST_ROOT $ENV{ODIN_BOOST_ROOT})
-    # official hint variable to set
-    set(BOOST_ROOT ${ODIN_BOOST_ROOT})
+# minimum Boost version
+set(ODIN_BOOST_VERSION 1.71)
+# find Boost. can set ODIN_BOOST_ROOT in environment or define, but we first
+# attempt to find it in case there is an existing system installation
+find_package(Boost ${ODIN_BOOST_VERSION})
+# if not found, rely on environment variable
+if(NOT Boost_FOUND)
+    if(DEFINED ENV{ODIN_BOOST_ROOT})
+        message(STATUS "Using ODIN_BOOST_ROOT $ENV{ODIN_BOOST_ROOT} from env")
+        set(ODIN_BOOST_ROOT $ENV{ODIN_BOOST_ROOT})
+        # official hint variable to set
+        set(BOOST_ROOT ${ODIN_BOOST_ROOT})
+    endif()
+    # must be defined unless BOOST_ROOT already defined
+    if(DEFINED ODIN_BOOST_ROOT)
+        message(
+            STATUS
+            "Using BOOST_ROOT from ODIN_BOOST_ROOT ${ODIN_BOOST_ROOT}"
+        )
+        set(BOOST_ROOT ${ODIN_BOOST_ROOT})
+    elseif(DEFINED BOOST_ROOT)
+        message(STATUS "BOOST_ROOT already defined as ${BOOST_ROOT}")
+    else()
+        message(FATAL_ERROR "ODIN_BOOST_ROOT, BOOST_ROOT not defined")
+    endif()
+    # now go find Boost
+    find_package(Boost ${ODIN_BOOST_VERSION} REQUIRED)
 endif()
-# must be defined unless BOOST_ROOT already defined
-if(DEFINED ODIN_BOOST_ROOT)
-    message(STATUS "Using BOOST_ROOT from ODIN_BOOST_ROOT ${ODIN_BOOST_ROOT}")
-    set(BOOST_ROOT ${ODIN_BOOST_ROOT})
-elseif(DEFINED BOOST_ROOT)
-    message(STATUS "BOOST_ROOT already defined as ${BOOST_ROOT}")
-else()
-    message(FATAL_ERROR "ODIN_BOOST_ROOT, BOOST_ROOT not defined")
-endif()
-# actual Boost find_package command + add to includes
-find_package(Boost 1.79 REQUIRED COMPONENTS)
+# add to includes
 include_directories(SYSTEM ${Boost_INCLUDE_DIRS})
 
-# find Google Test. can set ODIN_GTEST_ROOT in environment or define
-if(DEFINED ENV{ODIN_GTEST_ROOT})
-    message(STATUS "Using ODIN_GTEST_ROOT $ENV{ODIN_GTEST_ROOT} from env")
-    set(ODIN_GTEST_ROOT $ENV{ODIN_GTEST_ROOT})
-    # official hint variable to set
-    set(GTEST_ROOT ${ODIN_GTEST_ROOT})
+# minimum Google Test version
+set(ODIN_GTEST_VERSION 1.8)
+# find Google Test. can set ODIN_GTEST_ROOT in environment or define, but we
+# first attempt to find it in case there is an existing system installation
+find_package(GTest ${ODIN_GTEST_VERSION})
+# if not found, rely on environment variable
+if(NOT GTest_FOUND)
+    if(DEFINED ENV{ODIN_GTEST_ROOT})
+        message(STATUS "Using ODIN_GTEST_ROOT $ENV{ODIN_GTEST_ROOT} from env")
+        set(ODIN_GTEST_ROOT $ENV{ODIN_GTEST_ROOT})
+        # official hint variable to set
+        set(GTEST_ROOT ${ODIN_GTEST_ROOT})
+    endif()
+    # must be defined unless GTEST_ROOT already defined
+    if(DEFINED ODIN_GTEST_ROOT)
+        message(STATUS "GTEST_ROOT already defined as ${GTEST_ROOT}")
+    else()
+        message(FATAL_ERROR "ODIN_GTEST_ROOT, GTEST_ROOT not defined")
+    endif()
+    # actual Google Test find_package command
+    find_package(GTest ${ODIN_GTEST_VERSION} REQUIRED)
 endif()
-# must be defined unless GTEST_ROOT already defined
-if(DEFINED ODIN_GTEST_ROOT)
-    message(STATUS "GTEST_ROOT already defined as ${GTEST_ROOT}")
-else()
-    message(FATAL_ERROR "ODIN_GTEST_ROOT, GTEST_ROOT not defined")
-endif()
-# actual Google Test find_package command
-find_package(GTest 1.8 REQUIRED)
 # not really necessary, but by using SYSTEM we suppress external warnings
 include_directories(SYSTEM ${GTest_INCLUDE_DIRS})
