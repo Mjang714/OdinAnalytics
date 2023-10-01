ff11fa82fe8d9628754acfffa9f6bb1b99cf74e9
Michael Jang <mjang714@gmail.com>
2023-10-01 19:54:20 -0400

    Removed indents

--- src/derived_time/date_formula/date_formula.cpp
+++ src/derived_time/date_formula/date_formula.cpp
@@ -3,36 +3,35 @@
 #include "time/date_adjust/adjustment_factory.h"
 #include "static_data_cache/calendar_cache.h"
 
-namespace oa::derived_time
+namespace oa::derived_time {
+DateFormula::DateFormula() : m_tenor_("0D"), 
+	m_holiday_adjuster_(oa::time::AdjustmentFactory::CreateDateAdjust(oa::time::AdjRule::kPlainAdjustment))
 {
-	DateFormula::DateFormula() : m_tenor_("0D"), 
-		m_holiday_adjuster_(oa::time::AdjustmentFactory::CreateDateAdjust(oa::time::AdjRule::kPlainAdjustment))
-	{
 
-	}
-	DateFormula::DateFormula(const oa::time::Tenor& tenor_obj, oa::time::AdjRule adjustment_rule, const oa::time::Calendar& calendar) : m_tenor_(tenor_obj),
-		m_holiday_adjuster_(oa::time::AdjustmentFactory::CreateDateAdjust(adjustment_rule, calendar))
-	{
+}
+DateFormula::DateFormula(const oa::time::Tenor& tenor_obj, oa::time::AdjRule adjustment_rule, const oa::time::Calendar& calendar) : m_tenor_(tenor_obj),
+	m_holiday_adjuster_(oa::time::AdjustmentFactory::CreateDateAdjust(adjustment_rule, calendar))
+{
 
-	}
+}
 
-	DateFormula::DateFormula(const std::string& tenor_obj, oa::time::AdjRule adjustment_rule, const oa::time::Calendar& calendar) : m_tenor_(tenor_obj),
-		m_holiday_adjuster_(oa::time::AdjustmentFactory::CreateDateAdjust(adjustment_rule, calendar))
-	{
+DateFormula::DateFormula(const std::string& tenor_obj, oa::time::AdjRule adjustment_rule, const oa::time::Calendar& calendar) : m_tenor_(tenor_obj),
+	m_holiday_adjuster_(oa::time::AdjustmentFactory::CreateDateAdjust(adjustment_rule, calendar))
+{
 
-	}
+}
 
-	DateFormula::DateFormula(const std::string& tenor_obj, oa::time::AdjRule adjustment_rule, const std::string& calendar) : m_tenor_(tenor_obj),
-		m_holiday_adjuster_(oa::time::AdjustmentFactory::CreateDateAdjust(adjustment_rule, 
-			*oa::static_cache::CalendarCache::RetrieveCache().GetCalendar(calendar)))
-	{
+DateFormula::DateFormula(const std::string& tenor_obj, oa::time::AdjRule adjustment_rule, const std::string& calendar) : m_tenor_(tenor_obj),
+	m_holiday_adjuster_(oa::time::AdjustmentFactory::CreateDateAdjust(adjustment_rule, 
+		*oa::static_cache::CalendarCache::RetrieveCache().GetCalendar(calendar)))
+{
 
-	}
+}
 
-	oa::time::Date DateFormula::Adjust(const oa::time::Date& date_obj) const
-	{
-		auto result_date(date_obj);
+oa::time::Date DateFormula::Adjust(const oa::time::Date& date_obj) const
+{
+	auto result_date(date_obj);
 
-		return  m_holiday_adjuster_->AdjustDate(result_date.AddTenor(m_tenor_));
-	}
+	return  m_holiday_adjuster_->AdjustDate(result_date.AddTenor(m_tenor_));
+}
 }
--- src/derived_time/date_formula/date_formula.h
+++ src/derived_time/date_formula/date_formula.h
@@ -9,27 +9,26 @@
 #include "time/tenor.h"
 #include "time/date_adjust/date_adjust_base.h"
 
-namespace oa::derived_time
+namespace oa::derived_time {
+class OA_DERIVED_TIME_API DateFormula
 {
-	class OA_DERIVED_TIME_API DateFormula
-	{
-		public:
-			DateFormula();
-			DateFormula(const oa::time::Tenor& tenor_obj, oa::time::AdjRule adjustment_rule, const oa::time::Calendar& calendar);
-			DateFormula(const std::string& tenor_obj,  oa::time::AdjRule adjustment_rule, const oa::time::Calendar& calendar);
-			DateFormula(const std::string& tenor_obj, oa::time::AdjRule adjustment_rule, const std::string& calendar);
+	public:
+		DateFormula();
+		DateFormula(const oa::time::Tenor& tenor_obj, oa::time::AdjRule adjustment_rule, const oa::time::Calendar& calendar);
+		DateFormula(const std::string& tenor_obj,  oa::time::AdjRule adjustment_rule, const oa::time::Calendar& calendar);
+		DateFormula(const std::string& tenor_obj, oa::time::AdjRule adjustment_rule, const std::string& calendar);
 			
-			/// <summary>
-			/// Applies a tenor addtion and date adjustment to a date this allows us to apply date logic
-			/// </summary>
-			/// <param name="date_obj"></param>
-			/// <returns></returns>
-			oa::time::Date Adjust(const oa::time::Date& date_obj) const;
+		/// <summary>
+		/// Applies a tenor addtion and date adjustment to a date this allows us to apply date logic
+		/// </summary>
+		/// <param name="date_obj"></param>
+		/// <returns></returns>
+		oa::time::Date Adjust(const oa::time::Date& date_obj) const;
 
-		private:
-			oa::time::Tenor m_tenor_;
-			std::unique_ptr<oa::time::DateAdjustBase> m_holiday_adjuster_;  
-	};
+	private:
+		oa::time::Tenor m_tenor_;
+		std::unique_ptr<oa::time::DateAdjustBase> m_holiday_adjuster_;  
+};
 }
 
 #endif // !OA_DERIVED_TIME_DATE_FORMULA_H_
