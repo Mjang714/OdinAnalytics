faa36b2e444e02d9c7cfe0ab30872c8190b14127
Derek Huang <djh458@stern.nyu.edu>
2025-11-16 00:31:52 -0500

    add oa_config library with path lookup functions + admin tweaks
    modified:   .gitattributes
        Ensure that text files use LF by default
    
    new file:   include/oa/config/dllexport.h
        Add visibility macros for the oa_config library
    
    new file:   include/oa/config/paths.h
        Add helper functions for finding the location of the Odin library and
        shared data directories to avoid use of OdinBaseDir
    
    modified:   src/CMakeLists.txt
        Add config subdirectory to build
    
    new file:   src/config/CMakeLists.txt
        Add build config for oa_config
    
    new file:   src/config/paths.cpp
        Use GetModuleHandleA() + GetModuleFileNameA() on Windows and dladdr() on
        GNU/Linux (most LInux variants should be fine with this too)
    
    new file:   src/unit_test/config/paths_test.cpp
        Add simple unit test to check that the functions work properly

---
+++ src/config/paths.cpp
@@ -0,0 +1,98 @@
+/**
+ * @file config/paths.cpp
+ * @author Derek Huang
+ * @brief C++ source for library path helpers
+ * @copyright MIT License
+ */
+
+#include "oa/config/paths.h"
+
+// Windows
+#if defined(_WIN32)
+// reduce include size
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif  // WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <errhandlingapi.h>
+#include <libloaderapi.h>
+// POSIX/Linux
+#else
+// note: required by Linux but not BSD
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif  // _GNU_SOURCE
+#include <dlfcn.h>
+#endif  // !defined(_WIN32)
+
+// only need errno if compiling with _GNU_SOURCE here
+#ifdef _GNU_SOURCE
+#include <cerrno>
+#endif  // _GNU_SOURCE
+#include <filesystem>
+#include <stdexcept>
+#include <string>
+#include <system_error>
+
+#include "oa/rtti.h"
+
+namespace oa {
+namespace config {
+
+std::filesystem::path library_path()
+{
+#if defined(_WIN32)
+  // get module handle for DLL
+  // TODO: if we want to avoid hardcoding this name we can included a header
+  // configured by CMake that would have the DLL name
+  auto mod = GetModuleHandleA("oa_config");
+  if (!mod)
+    throw std::system_error{
+      // note: cast required to avoid narrowing conversion from DWORD
+      {static_cast<int>(GetLastError()), std::system_category()},
+      "unable to get oa_config module handle"
+    };
+  // get absolute path name
+  char name[MAX_PATH];
+  auto name_len = GetModuleFileNameA(mod, name, MAX_PATH);
+  // handle any errors
+  if (!name_len)
+    throw std::system_error{
+      {static_cast<int>(GetLastError()), std::system_category()},
+      "unable to get oa_config module file name"
+    };
+  // done, return absolute path
+  // note: using {begin, end} ctor overload since Windows XP doesn't
+  // null-terminate the returned character buffer on truncation
+  return {name, name + name_len};
+#else
+  // use address of library_path() itself to get path of oa_config
+  Dl_info info;
+  // note: shouldn't fail since library_path() is part of the DSO itself
+  if (!dladdr(reinterpret_cast<const void*>(&library_path), &info))
+    throw std::system_error{
+      {errno, std::system_category()},
+      OA_PRETTY_FUNCTION_NAME + std::string{": dladdr() failed"}
+    };
+  // return absolute path
+  return info.dli_fname;
+#endif  // !defined(_WIN32)
+}
+
+std::filesystem::path library_dir()
+{
+  return library_path().parent_path();
+}
+
+std::filesystem::path data_dir()
+{
+  // path to library directory
+  auto lib_dir = library_dir();
+  // preferred path to shared data
+  auto share_dir = lib_dir.parent_path() / "share" / "oa";
+  // if share_dir doesn't exist use lib_dir
+  return (std::filesystem::exists(share_dir)) ? share_dir : lib_dir;
+}
+
+}  // namespace config
+}  // namespace oa
