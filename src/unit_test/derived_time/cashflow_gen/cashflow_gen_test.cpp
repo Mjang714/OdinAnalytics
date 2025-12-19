b561b56c6c2f718c0a0a9b72680475d252df2d3b
Michael Jang <mjang714@gmail.com>
2025-12-19 12:09:42 -0500

    Finished adding unit test to test fixing reset and payment lag

--- src/unit_test/derived_time/cashflow_gen/cashflow_gen_test.cpp
+++ src/unit_test/derived_time/cashflow_gen/cashflow_gen_test.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include "oa/testing/gtest.h"
 #include <gmock/gmock.h>
 
 #include "time/date.h"
@@ -23,7 +24,14 @@ namespace
 				  frequency("6M"),
 				  notional(0.0),
 				  rate(0.0),
-				  day_cnt_rule(oa::time::DayCountRule::kACT_360)
+				  day_cnt_rule(oa::time::DayCountRule::kACT_360),
+				  tenor_fix("-2d"),
+				  fix_calendar("NYB"),
+				  tenor_pay("2d"),
+				  pay_calendar("NYB"),
+				  adjustment_rule_pay(oa::time::AdjRule::kFollowing),
+				  adjustment_rule_fix(oa::time::AdjRule::kPreceding)
+
 			{
 			}
 
@@ -34,6 +42,13 @@ namespace
 			double notional;
 			double rate;
 			oa::time::DayCountRule day_cnt_rule;
+			oa::derived_time::DateFormula date_formula_fix, date_formula_pay;
+			std::string pay_calendar;
+			std::string fix_calendar;
+			std::string tenor_pay;
+			std::string tenor_fix;
+			oa::time::AdjRule adjustment_rule_pay;
+			oa::time::AdjRule adjustment_rule_fix;
 			
 			std::vector<oa::derived_time::CashflowStruct> fixed_cf_base{
 				{"2025-1-3","2025-7-3","2025-1-3","2025-7-3","2025-1-3","2025-7-3", 1000000.0, .05, 0.0, 0, 0, 0.0, dt::Currency::kUSD, dt::CashflowType::kFixed},
@@ -50,8 +65,24 @@ namespace
 
 			};
 
+			std::vector<oa::derived_time::CashflowStruct> fixed_cf_fix_pay_adj{
+				{"2025-1-3","2025-7-3","2025-1-3","2025-7-3","2024-12-31","2025-7-7", 1000000.0, .05, 0.0, 0, 0, 0.0, dt::Currency::kUSD, dt::CashflowType::kFixed},
+				{"2025-7-3","2026-1-3","2025-7-3","2026-1-3","2025-7-1","2026-1-5", 1000000.0, .05, 0.0, 0, 0, 0.0, dt::Currency::kUSD, dt::CashflowType::kFixed},
+				{"2026-1-3","2026-7-3","2026-1-3","2026-7-3","2025-12-31","2026-7-6", 1000000.0, .05, 0.0, 0, 0, 0.0, dt::Currency::kUSD, dt::CashflowType::kFixed},
+				{"2026-7-3","2027-1-3","2026-7-3","2027-1-3","2026-7-1","2027-1-5", 1000000.0, .05, 0.0, 0, 0, 0.0, dt::Currency::kUSD, dt::CashflowType::kFixed },
+				{"2027-1-3","2027-7-3","2027-1-3","2027-7-3","2026-12-31","2027-7-6", 1000000.0, .05, 0.0, 0, 0, 0.0, dt::Currency::kUSD, dt::CashflowType::kFixed },
+				{"2027-7-3","2028-1-3","2027-7-3","2028-1-3","2027-7-1","2028-1-5", 1000000.0, .05, 0.0, 0, 0, 0.0, dt::Currency::kUSD, dt::CashflowType::kFixed },
+				{"2028-1-3","2028-7-3","2028-1-3","2028-7-3","2027-12-31","2028-7-5", 1000000.0, .05, 0.0, 0, 0, 0.0, dt::Currency::kUSD, dt::CashflowType::kFixed },
+				{"2028-7-3","2029-1-3","2028-7-3","2029-1-3","2028-6-30","2029-1-5", 1000000.0, .05, 0.0, 0, 0, 0.0, dt::Currency::kUSD, dt::CashflowType::kFixed },
+				{"2029-1-3","2029-7-3","2029-1-3","2029-7-3","2028-12-29","2029-7-5", 1000000.0, .05, 0.0, 0, 0, 0.0, dt::Currency::kUSD, dt::CashflowType::kFixed },
+				{"2029-7-3","2030-1-3","2029-7-3","2030-1-3","2029-6-29","2030-1-7", 1000000.0, .05, 0.0, 0, 0, 0.0, dt::Currency::kUSD, dt::CashflowType::kFixed },
+				{"2029-7-3","2030-1-3","2029-7-3","2030-1-3","2029-6-29","2030-1-7", 1000000.0, 1.0, 0.0, 0, 0, 0.0, dt::Currency::kUSD, dt::CashflowType::kPrincipal }
+
+			};
+
 			virtual void SetUp() override
 			{
+				OA_GTEST_ENSURE_BASE_DIR();
 				start_date = oa::time::Date(2025, 1, 3);
 				mat_date = oa::time::Date(2030, 1, 3);
 				frequency = oa::time::Tenor("6M");
@@ -66,6 +97,13 @@ namespace
 					cf.cashflow_amount = cf.notional * (cf.rate * cf.day_count_fraction);
 				}
 				fixed_cf_base.at(10).cashflow_amount = notional;
+
+				for (auto& cf : fixed_cf_fix_pay_adj) {
+					cf.days = day_count->DayCount(cf.start_date, cf.end_date);
+					cf.day_count_fraction = day_count->YearFraction(cf.start_date, cf.end_date);
+					cf.cashflow_amount = cf.notional * (cf.rate * cf.day_count_fraction);
+				}
+				fixed_cf_fix_pay_adj.at(10).cashflow_amount = notional;
 			}
 			virtual void TearDown() override
 			{
@@ -89,7 +127,7 @@ namespace
 			oa::derived_time::Currency::kUSD,
 			oa::derived_time::DateDirection::kForward
 		);
-		EXPECT_THAT(cashflows, ::testing::Pointwise(::testing::Eq(), fixed_cf_base));
+		EXPECT_THAT(fixed_cf_base, ::testing::Pointwise(::testing::Eq(), cashflows));
 	}
 
 	/// <summary>
@@ -109,6 +147,52 @@ namespace
 			oa::derived_time::Currency::kUSD,
 			oa::derived_time::DateDirection::kBackward
 		);
-		EXPECT_THAT(cashflows, ::testing::Pointwise(::testing::Eq(), fixed_cf_base));
+		EXPECT_THAT(fixed_cf_base, ::testing::Pointwise(::testing::Eq(), cashflows));
+	}
+
+	TEST_F(CashflowGenBaseTest, CreateFixedCashflowsFwdTestWithPayFixAdj)
+	{
+		auto cashflows = oa::derived_time::CashflowGen::CreateFixedCashflows(
+			start_date,
+			mat_date,
+			oa::derived_time::Frequency::kSemiAnnual,
+			notional,
+			rate,
+			day_cnt_rule,
+			oa::derived_time::Currency::kUSD,
+			oa::derived_time::DateDirection::kForward,
+			oa::derived_time::CashflowType::kFixed,
+			oa::derived_time::ResetDirection::kAdvance,
+			oa::derived_time::StubType::kNone,
+			std::nullopt,
+			std::nullopt,
+			oa::derived_time::DateFormula(tenor_pay, adjustment_rule_pay, pay_calendar),
+			oa::derived_time::DateFormula(tenor_fix, adjustment_rule_fix, fix_calendar)
+
+		);
+		EXPECT_THAT(fixed_cf_fix_pay_adj, ::testing::Pointwise(::testing::Eq(), cashflows));
+	}
+
+	TEST_F(CashflowGenBaseTest, CreateFixedCashflowsBkwdTestWithPayFixAdj)
+	{
+		auto cashflows = oa::derived_time::CashflowGen::CreateFixedCashflows(
+			start_date,
+			mat_date,
+			oa::derived_time::Frequency::kSemiAnnual,
+			notional,
+			rate,
+			day_cnt_rule,
+			oa::derived_time::Currency::kUSD,
+			oa::derived_time::DateDirection::kBackward,
+			oa::derived_time::CashflowType::kFixed,
+			oa::derived_time::ResetDirection::kAdvance,
+			oa::derived_time::StubType::kNone,
+			std::nullopt,
+			std::nullopt,
+			oa::derived_time::DateFormula(tenor_pay, adjustment_rule_pay ,pay_calendar),
+			oa::derived_time::DateFormula(tenor_fix, adjustment_rule_fix ,fix_calendar)
+
+		);
+		EXPECT_THAT(fixed_cf_fix_pay_adj, ::testing::Pointwise(::testing::Eq(), cashflows));
 	}
 }
