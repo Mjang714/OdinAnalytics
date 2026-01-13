1fe6390f23c3c077391e0149335ebf503dfc8cc9
Derek Huang <djh458@stern.nyu.edu>
2026-01-13 01:10:11 -0500

    add operator<< for xloper12
    modified:   include/oa/accel/xl_ops.h
    modified:   src/accel/xl_ops.cpp

--- include/oa/accel/xl_ops.h
+++ include/oa/accel/xl_ops.h
@@ -11,6 +11,7 @@
 #include <iosfwd>
 
 // forward decls to avoid pulling in XLCALL.H
+struct xloper12;
 struct xlref12;
 
 /**
@@ -44,5 +45,30 @@ bool operator!=(const xlref12& a, const xlref12& b) noexcept;
  * @param ref Excel cell reference to stream
  */
 std::ostream& operator<<(std::ostream& out, const xlref12& ref);
+
+/**
+ * Stream operator for a `xloper12`.
+ *
+ * This uses any available `operator<<` defined for the relevant `XLCALL.H`
+ * type and is called recursively for `xltypeMulti` objects. The `xltypeMulti`
+ * objects will always be displayed as 2D arrays.
+ *
+ * @note `xltypeStr` data will be narrowed so some Unicode code points may not
+ *  display as expected when written to the narrow stream.
+ *
+ * @par
+ *
+ * @note `xltypeBigData` data makes the assumption that the `XLOPER12` was
+ *  zeroed before initialization and so treats zero `cbData` as an indication
+ *  that the `HANDLE` should be formatted instead of the `BYTE*` in the union.
+ *
+ * @par
+ *
+ * @todo `xltypeFlow` is not yet supported and returns just the type + address.
+ *
+ * @param out Output stream
+ * @param op `XLOPER12` to write to stream
+ */
+std::ostream& operator<<(std::ostream& out, const xloper12& op);
 
 #endif  // OA_ACCEL_XL_OPS_H_
--- src/accel/xl_ops.cpp
+++ src/accel/xl_ops.cpp
@@ -13,7 +13,11 @@
 #include <Windows.h>
 #include <XLCALL.H>
 
+#include <cstdint>
+#include <iostream>     // for std::wcout
 #include <ostream>
+
+#include "oa/string.h"  // for oa::hex
 
 ////////////////////////////////////////////////////////////////////////////////
 // xlref12                                                                    //
@@ -43,4 +47,120 @@ std::ostream& operator<<(std::ostream& out, const xlref12& ref)
     return out;
   // otherwise multi-cell reference, so stream bottom-right cell
   return out << " ... (" << ref.rwLast << ", " << ref.colLast << ")";
+}
+
+std::ostream& operator<<(std::ostream& out, const xloper12& op)
+{
+  // switch on type
+  switch (op.xltype) {
+  // numeric
+  case xltypeNum:
+    out << op.val.num;
+    break;
+  // string
+  case xltypeStr:
+    out << '"';
+    // note: size is first element
+    for (auto i = 0u; i < static_cast<unsigned>(op.val.str[0]); i++)
+      out << std::wcout.narrow(op.val.str[i + 1], '?');
+    out << '"';
+    break;
+  // boolean
+  case xltypeBool:
+    out << op.val.xbool;
+    break;
+  // error
+  case xltypeErr:
+    // TODO: split into separate function
+    switch (op.val.err) {
+    case xlerrNull:
+      out << "#NULL!";
+      break;
+    case xlerrDiv0:
+      out << "#DIV/0!";
+      break;
+    case xlerrValue:
+      out << "#VALUE!";
+      break;
+    case xlerrRef:
+      out << "#REF!";
+      break;
+    case xlerrName:
+      out << "#NAME?";
+      break;
+    case xlerrNum:
+      out << "#NUM!";
+      break;
+    case xlerrNA:
+      out << "#N/A";
+      break;
+    case xlerrGettingData:
+      out << "#WAIT!";
+      break;
+    }
+    break;
+  // integer
+  case xltypeInt:
+    out << op.val.w;
+    break;
+  // single reference
+  case xltypeSRef:
+    out << op.val.sref.ref;
+    break;
+  // multi reference
+  case xltypeRef:
+    // TODO: kind of duplicates the mref12 operator<<
+    out << oa::hex << op.val.mref.idSheet << ": ";
+    out << "[";
+    for (auto i = 0u; i < op.val.mref.lpmref->count; i++) {
+      if (i)
+        out << ", ";
+      out << op.val.mref.lpmref->reftbl[i];
+    }
+    out << "]";
+    break;
+  // array
+  // note: always formatted as 2D for now
+  case xltypeMulti:
+    out << "[";
+    for (RW i = 0; i < op.val.array.rows; i++) {
+      out << "[";
+      for (COL j = 0; j < op.val.array.columns; j++) {
+        if (j)
+          out << ", ";
+        out << op.val.array.lparray[i * op.val.array.rows + j];
+      }
+      out << "]";
+    }
+    out << "]";
+    break;
+  // flow
+  // TODO: not implemented
+  case xltypeFlow:
+    out << "<xltypeFlow at 0x" << oa::hex <<
+      reinterpret_cast<std::uintptr_t>(&op) << ">";
+    break;
+  // bigdata
+  case xltypeBigData:
+    // if data size is zero assume this is holding a handle from Excel
+    if (!op.val.bigdata.cbData)
+      out << "<xltypeBigData (HANDLE) at 0x" << oa::hex <<
+        reinterpret_cast<std::uintptr_t>(op.val.bigdata.h.hdata) << ">";
+    // otherwise, indicate data + length
+    else
+      out << "<xltypeBigData (BYTE*, " << op.val.bigdata.cbData << ") at 0x" <<
+        oa::hex <<
+        reinterpret_cast<std::uintptr_t>(op.val.bigdata.h.lpbData) << ">";
+    break;
+  // missing
+  case xltypeMissing:
+    out << "missing";
+    break;
+  // nil
+  case xltypeNil:
+    out << "nil";
+    break;
+  }
+  // chaining
+  return out;
 }
