859ec04849841adc6a9142ba513b15829c914c1c
Derek Huang <djh458@stern.nyu.edu>
2026-01-20 04:02:00 -0500

    accel_demo.cpp: test wrapping some simple functions

--- test/accel_demo.cpp
+++ test/accel_demo.cpp
@@ -8,6 +8,7 @@
 #include <string>
 
 #include "oa/accel/addin.h"
+#include "oa/accel/oper12.h"
 #include "oa/config/paths.h"
 #include "oa/version.h"
 
@@ -29,7 +30,41 @@ OA_XLL_EXPORT(const char*) OaDataDir()
 
 OA_ACCEL_EXPORT_FUNC(OaDataDir)
   .category("OA Config")
-  .help("Return the path to the OA static data directory")
-  .volatile_(true);
+  .help("Return the path to the OA static data directory");
+
+/**
+ * Return the maximum of the two values.
+ */
+OA_XLL_EXPORT(double) OaMax(double a, double b)
+{
+  return (a > b) ? a : b;
+}
+
+OA_ACCEL_EXPORT_FUNC(OaMax)
+  .category("OA Math")
+  .help("Return the maximum of the two values")
+  .arg("a", "First value")
+  .arg("b", "Second value");
+
+/**
+ * Convert all the characters in the string to upper case.
+ */
+OA_XLL_EXPORT(xloper12*) OaToUpper(const char* in) OA_ACCEL_SAFE()
+{
+  std::string str{in};
+  // change to uppercase as appropriate
+  for (auto& c : str)
+    if (c >= 'a' && c <= 'z')
+      c -= ('a' - 'A');
+  // provide back to Excel
+  OA_ACCEL_SAFE_RETURN(accel::oper12{str}.release());
+}
+
+OA_ACCEL_EXPORT_FUNC(OaToUpper)
+  .category("OA String")
+  .help("Convert lowercase characters in the string to upper case")
+  .arg("s", "Input string");
+
+// TODO: add naive matmul
 
 }  // namespace oa
