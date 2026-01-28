6aa87cc9d6806d9c3aa02ce759595918782fcff6
Derek Huang <djh458@stern.nyu.edu>
2026-01-27 23:46:30 -0500

    axl_demo.cpp: factor dict logic into to_oxl_dict() + add AxAddBusinessDays()

--- test/axl_demo.cpp
+++ test/axl_demo.cpp
@@ -230,52 +230,89 @@ OA_ACCEL_EXPORT_FUNC(AxYearFraction)
 // names since two places would have to be updated at once.
 //
 
+namespace {
+
+/**
+ * Convert keys + values, a full key-value mapping, or a handle to an OXL dict.
+ *
+ * This implements most of the flexible parameter conversion.
+ *
+ * @param a1 Input keys, key-value rows, or a string handle
+ * @param a2 Input values or omitted
+ */
+auto to_oxl_dict(/*const*/ xloper12* a1, /*const*/ xloper12* a2)
+{
+  using oxl::xl_api::XLoperObj;
+  using oxl::xl_api::XlCacheObj;
+  // get views of opaque type
+  accel::oper12_view v1{a1};
+  accel::oper12_view v2{a2};
+  // if v1 + v2 are multi, assume v1 has keys, v2 has values
+  if (v1.type() == accel::xltype::multi && v2.type() == accel::xltype::multi)
+    return XLoperObj::LPXloperToDictionary(a1, a2);
+  // if v1 is string, assume it is a handle
+  if (v1.type() == accel::xltype::str) {
+    // get cache key from handle + cache object
+    auto key = XlCacheObj::GetKeyFromHandle(v1.as<std::string>());
+    auto obj = XlCacheObj::GetVariant(key);
+    // return dictionary (throws if not actually a dictionary)
+    return *std::get<std::shared_ptr<oxl::xl_api::XlDictionary>>(obj);
+  }
+  // otherwise, assume v1 has keys and values
+  return XLoperObj::LPXloperToDictionary(a1);
+}
+
+}  // namespace
+
+/**
+ * Compute a new date given a base date, tenor, adjustment rule, and calendar.
+ */
 OA_XLL_EXPORT() AxComputeDate(
   /*const*/ xloper12* a1,
   /*const*/ xloper12* a2) OA_ACCEL_SAFE()
 {
-  // convert to XlDictionary input
-  auto dict = [a1, a2]
-  {
-    using oxl::xl_api::XLoperObj;
-    using oxl::xl_api::XlCacheObj;
-    // get views of opaque type
-    accel::oper12_view v1{a1};
-    accel::oper12_view v2{a2};
-    // if v1 + v2 are multi, assume v1 has keys, v2 has values
-    if (v1.type() == accel::xltype::multi && v2.type() == accel::xltype::multi)
-      return XLoperObj::LPXloperToDictionary(a1, a2);
-    // if v1 is string, assume it is a handle
-    if (v1.type() == accel::xltype::str) {
-      // get cache key from handle + cache object
-      auto key = XlCacheObj::GetKeyFromHandle(v1.as<std::string>());
-      auto obj = XlCacheObj::GetVariant(key);
-      // return dictionary (throws if not actually a dictionary)
-      return *std::get<std::shared_ptr<oxl::xl_api::XlDictionary>>(obj);
-    }
-    // otherwise, assume v1 has keys and values
-    return XLoperObj::LPXloperToDictionary(a1);
-  }();
-  // return new date
-  OA_ACCEL_SAFE_RETURN(oxl::OxlComputeDate(dict));
+  OA_ACCEL_SAFE_RETURN(oxl::OxlComputeDate(to_oxl_dict(a1, a2)));
 }
 
 OA_ACCEL_EXPORT_FUNC(AxComputeDate)
   .category("AXL Time")
-  // TODO: have web documentation since the help is limited to 255 chars here
+  // TODO: have web documentation since the help is limited to 255 chars
   .help(
-    "Compute a new date given a base date, tenor, calendar, and modifier.\n"
+    "Compute a new date given a base date, tenor, adjustment rule, and "
+      "calendar.\n"
     "\n"
-    "The input has the following key-value input format:\n"
+    "The input has the following key-value format:\n"
     "\n"
     "Base_Date: date, e.g. 2024-05-06\n"
     "Tenor: tenor, e.g. \"1Y\"\n"
     "Adjustment_Rule: rule, e.g. \"Following\"\n"
     "Calendar: calendar, e.g. \"NYB\""
   )
-  .arg("keys_or_dict", "Rows of input keys or key + value rows")
-  .arg("values", "Rows of input values (may be omitted)");
+  .arg("keys_or_dict", "Rows of input keys, key + value rows, or a handle")
+  .arg("maybe_values", "Rows of input values (may be omitted)");
 
-// TODO: add AxAddBusinessDays()
+/**
+ * Add a given number of business days to a starting date.
+ */
+OA_XLL_EXPORT() AxAddBusinessDays(
+  /*const*/ xloper12* a1,
+  /*const*/ xloper12* a2) OA_ACCEL_SAFE()
+{
+  OA_ACCEL_SAFE_RETURN(oxl::OxlAddBusinessDays(to_oxl_dict(a1, a2)));
+}
+
+OA_ACCEL_EXPORT_FUNC(AxAddBusinessDays)
+  .category("AXL Time")
+  .help(
+    "Add the specified number of business days to the starting date.\n"
+    "\n"
+    "The input has the following key-value format:\n"
+    "\n"
+    "Base_Date: date, e.g. 2024-05-06\n"
+    "Days: days, e.g. 5\n"
+    "Calendar: calendar, e.g. \"NYB\""
+  )
+  .arg("keys_or_dict", "Rows of input keys, key + value rows, or a handle")
+  .arg("maybe_values", "Rows of input values (may be omitted)");
 
 }  // namespace oa
