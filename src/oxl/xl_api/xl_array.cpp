94e4745766f1af286b14378ff9c244c6c29d9902
Michael Jang <Mjang714@gmail.com>
2026-03-07 19:02:59 -0500

    Revert "add row operations to XlArray + enhance"

--- src/oxl/xl_api/xl_array.cpp
+++ src/oxl/xl_api/xl_array.cpp
@@ -1,178 +1,20 @@
 #include "xl_array.h"
 
-#include <algorithm>
-#include <cstddef>
-#include <initializer_list>
-#include <stdexcept>
-#include <string>
-#include <utility>
 #include <vector>
-
-#include "xl_variant.h"
 
 namespace oxl::xl_api
 {
-
-////////////////////////////////////////////////////////////////////////////////
-// XlArray::RowView                                                           //
-////////////////////////////////////////////////////////////////////////////////
-
-XlArray::RowView::RowView(RowData& data) noexcept : data_{&data} {}
-
-std::size_t
-XlArray::RowView::size() const noexcept
-{
-	return data_->size();
-}
-
-XlArray::RowView::Iter
-XlArray::RowView::begin() const noexcept
-{
-	return data_->begin();
-}
-
-XlArray::RowView::Iter
-XlArray::RowView::end() const noexcept
-{
-	return data_->end();
-}
-
-XlVariant&
-XlArray::RowView::operator[](std::size_t i) const
-{
-	return data_->at(i);
-}
-
-bool
-XlArray::RowView::operator==(RowView view) const
-{
-	// delegate to std::vector<T>::operator==
-	return *data_ == *view.data_;
-}
-
-bool
-XlArray::RowView::operator==(CRowView view) const
-{
-	// per-element check
-	return std::ranges::equal(*this, view);
-}
-
-XlArray::RowView&
-XlArray::RowView::operator=(RowData data)
-{
-	// size mismatch
-	if (data.size() != data_->size())
-		throw std::invalid_argument{
-			"new row data size " + std::to_string(data.size()) +
-			" != expected row data size " + std::to_string(data_->size())
-		};
-	// otherwise, move-assign
-	*data_ = std::move(data);
-	return *this;
-}
-
-const XlArray::RowData&
-XlArray::RowView::operator*() const noexcept
-{
-	return *data_;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// XlArray::CRowView                                                          //
-////////////////////////////////////////////////////////////////////////////////
-
-XlArray::CRowView::CRowView(const RowData& data) noexcept : data_{&data} {}
-
-std::size_t
-XlArray::CRowView::size() const noexcept
-{
-	return data_->size();
-}
-
-XlArray::CRowView::Iter
-XlArray::CRowView::begin() const noexcept
-{
-	return data_->begin();
-}
-
-XlArray::CRowView::Iter
-XlArray::CRowView::end() const noexcept
-{
-	return data_->end();
-}
-
-const XlVariant&
-XlArray::CRowView::operator[](std::size_t i) const
-{
-	return data_->at(i);
-}
-
-bool
-XlArray::CRowView::operator==(CRowView view) const
-{
-	// delegate to std::vector<T>::operator==
-	return *data_ == *view.data_;
-}
-
-bool
-XlArray::CRowView::operator==(RowView view) const
-{
-	// per-element check
-	return std::ranges::equal(*this, view);
-}
-
-const XlArray::RowData&
-XlArray::CRowView::operator*() const noexcept
-{
-	return *data_;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// XlArray                                                                    //
-////////////////////////////////////////////////////////////////////////////////
-
-	XlArray::XlArray(size_t rows, size_t cols, const XlVariant& var)
-	  : m_rows_(rows), m_cols_(cols), m_data_(rows, RowData(cols, var))
+	XlArray::XlArray(size_t rows, size_t cols) : m_rows_(rows), m_cols_(cols), m_data_(rows,std::vector<XlVariant>(cols))
 	{
 
 	}
 
-	XlArray::XlArray(std::initializer_list<std::initializer_list<XlVariant>> data)
-	{
-		// require at least one row
-		if (data.size() < 1u)
-			throw std::invalid_argument{"at least 1 row required"};
-		// otherwise, set dimensions
-		m_rows_ = data.size();
-		m_cols_ = data.begin()->size();
-		// require at least one col
-		if (m_cols_ < 1u)
-			throw std::invalid_argument{"at least 1 col required"};
-		// current row index
-		// set values with dimension checking
-		std::size_t i = 0u;
-		for (auto row : data) {
-			// disallow ragged arrays
-			if (row.size() != m_cols_)
-				throw std::invalid_argument{
-					"row " + std::to_string(i) + " has size " +
-					std::to_string(row.size()) + " != expected row size " +
-					std::to_string(m_cols_)
-				};
-			// copy values + advance
-			m_data_.emplace_back(row);
-			i++;
-		}
-	}
-
-	XlArray::RowView
-	XlArray::operator[](size_t index)
+	std::vector<XlVariant>& XlArray::operator[] (size_t index)
 	{
 		return m_data_.at(index);
 	}
 
-	XlArray::CRowView
-	XlArray::operator[](const size_t index) const
+	const std::vector<XlVariant>& XlArray::operator[] (const size_t index) const
 	{
 		return m_data_.at(index);
 	}
@@ -187,8 +29,7 @@ XlArray::CRowView::operator*() const noexcept
 		return m_data_.at(row).at(cols);
 	}
 
-	// FIXME: clean up implementation
-	std::vector<XlVariant> XlArray::ToVector() const
+	std::vector<XlVariant> XlArray::ToVector()
 	{
 		if ((m_rows_ != 1) && (m_cols_ != 1))
 		{
--- src/oxl/xl_api/xl_array.h
+++ src/oxl/xl_api/xl_array.h
@@ -1,265 +1,61 @@
 #ifndef OXL_XL_API_XL_ARRAY_H_
 #define OXL_XL_API_XL_ARRAY_H_
 
-#include <cstddef>
-#include <initializer_list>
 #include <vector>
 
 #include "xl_variant.h"
 
 namespace oxl::xl_api
 {
-
-	/**
-	 * `XlVariant` array type.
-	 *
-	 * This is an intermediary class that assists in converting data between
-	 * the C++ and Excel layers via the `XLOperObj` members. To set and view
-	 * entire `XlArray` rows, use the `operator[]` overloads:
-	 *
-	 * @code{.cc}
-	 * // assuming arr is an XlArray shape (4, 4)
-	 * arr[0] = {1., "hello", true, 100.};
-	 * @endcode
-	 *
-	 * Rows can also be iterated through like a normal random-access container:
-	 *
-	 * @code{.cc}
-	 * // assuming arr is an XlArray shape (4, 4)
-	 * for (auto& value : arr[0])
-	 *   ;
-	 * @endcode
-	 *
-	 * The row data itself can be copy-assigned from `operator*`:
-	 *
-	 * @code{.cc}
-	 * // assuming arr is an XlArray shape (4, 4)
-	 * auto row = *arr[0];
-	 * // row is unchanged after assigning values
-	 * arr[0] = {2., "a", false, 50.};
-	 * assert(row[1] != arr[0][1]);
-	 * @endcode
-	 */
+	/// <summary>
+	/// this is an intermediary class that converts data from c++ to excel layer via the XLOperObj
+	/// </summary>
 	class XlArray
 	{
-	public:
-		// row data type
-		using RowData = std::vector<XlVariant>;
-
-		// forward decl for RowView::operator==
-		class CRowView;
-
-		/**
-		 * Modifiable view of the `XlArray` row data.
-		 */
-		class RowView {
 		public:
-			using Iter = RowData::iterator;
-			using value_type = XlVariant;    // EXPECT_THAT() compatibility
+			XlArray(size_t rows, size_t cols);
+			/// <summary>
+			/// returns an entire row of the array
+			/// </summary>
+			/// <param name="i"></param>
+			/// <returns></returns>
+			std::vector<XlVariant>& operator [](size_t i);
 
-			/**
-			 * Ctor.
-			 *
-			 * @param data Row data
-			 */
-			RowView(RowData& data) noexcept;
+			/// <summary>
+			/// this is const method to return an entire row
+			/// </summary>
+			/// <param name="i"></param>
+			/// <returns></returns>
+			const std::vector<XlVariant>& operator [](const size_t i) const;
 
-			/**
-			 * Return the number of elements in the row.
-			 */
-			std::size_t size() const noexcept;
+			/// <summary>
+			/// operation to retrieve an element of an array that is mutable
+			/// </summary>
+			/// <param name="row"></param>
+			/// <param name="col"></param>
+			/// <returns></returns>
+			XlVariant& operator () (size_t row,  size_t col);
 
-			/**
-			 * Return an iterator to the first row element.
-			 */
-			Iter begin() const noexcept;
+			/// <summary>
+			/// operation to retrieve an element of an array that is inmutable
+			/// </summary>
+			/// <param name="row"></param>
+			/// <param name="col"></param>
+			/// <returns></returns>
+			const XlVariant& operator () (const size_t row, const size_t col) const;
 
-			/**
-			 * Return an iterator one past the last row element.
-			 */
-			Iter end() const noexcept;
+			std::vector<XlVariant> ToVector();
 
-			/**
-			 * Return a reference to the specified row element.
-			 *
-			 * Indexing out of bounds will throw an exception.
-			 *
-			 * @param i Element index
-			 */
-			XlVariant& operator[](std::size_t i) const;
-
-			/**
-			 * Compare row elements for equality.
-			 *
-			 * @param view Row view
-			 */
-			bool operator==(RowView view) const;
-
-			/**
-			 * Compare row elements for equality.
-			 *
-			 * @param view Row view
-			 */
-			bool operator==(CRowView view) const;
-
-			/**
-			 * Return a const reference to the row data.
-			 *
-			 * This is useful when a direct reference to the row data is needed
-			 * but is mostly intended to enable succinct copying.
-			 */
-			const RowData& operator*() const noexcept;
-
-			/**
-			 * Assign new row data.
-			 *
-			 * If the new data dimensions don't match an exception is thrown.
-			 *
-			 * @param data New row data
-			 */
-			RowView& operator=(RowData data);
+			size_t rows() const { return m_rows_; };
+			size_t cols() const { return m_cols_; };
 
 		private:
-			RowData* data_;  // trivially copyable
-		};
-
-		/**
-		 * Non-modifiable view of the `XlArray` row data.
-		 */
-		class CRowView {
-		public:
-			using Iter = RowData::const_iterator;
-			using value_type = XlVariant;    // EXPECT_THAT() compatibility
-
-			/**
-			 * Ctor.
-			 *
-			 * @param data Row data
-			 */
-			CRowView(const RowData& data) noexcept;
-
-			/**
-			 * Return the number of elements in the row.
-			 */
-			std::size_t size() const noexcept;
-
-			/**
-			 * Return an iterator to the first const-qualified row element.
-			 */
-			Iter begin() const noexcept;
-
-			/**
-			 * Return an iterator one past the last const-qualified row element.
-			 */
-			Iter end() const noexcept;
-
-			/**
-			 * Return a const reference to the specified row element.
-			 *
-			 * Indexing out of bounds will throw an exception.
-			 *
-			 * @param i Element index
-			 */
-			const XlVariant& operator[](std::size_t i) const;
-
-			/**
-			 * Compare row elements for equality.
-			 *
-			 * @param view Row view
-			 */
-			bool operator==(CRowView view) const;
-
-			/**
-			 * Compare row elements for equality.
-			 *
-			 * @param view Row view
-			 */
-			bool operator==(RowView view) const;
-
-			/**
-			 * Return a const reference to the row data.
-			 *
-			 * This is useful when a direct reference to the row data is needed
-			 * but is mostly intended to enable succinct copying.
-			 */
-			const RowData& operator*() const noexcept;
-
-		private:
-			const RowData* data_;  // trivially copyable
-		};
-
-		/**
-		 * Ctor.
-		 *
-		 * This initializes an `XlArray` of the given dimensions with the
-		 * specified `XlVariant` values. The defaulted `XlVariant` will contain
-		 * a value-initialized value of its first alternative.
-		 *
-		 * @param rows Number of rows
-		 * @param cols Number of columns
-		 * @param var Matrix init value
-		 */
-		XlArray(size_t rows, size_t cols, const XlVariant& var = {});
-
-		/**
-		 * Ctor.
-		 *
-		 * Constructs from a non-ragged list of initializer lists, e.g.
-		 *
-		 * @code{.cc}
-		 * XlArray arr{
-		 *   {1., "hello", true},
-		 *   {2., "world", false}
-		 *   {true, 3., "beta"}
-		 * };
-		 * @endcode
-		 *
-		 * @param data Nested init-lists of elements
-		 */
-		XlArray(std::initializer_list<std::initializer_list<XlVariant>> data);
-
-		/**
-		 * Returns a modifiable view of the specified row.
-		 *
-		 * @param i Row index
-		 */
-		RowView operator[](size_t i);
-
-		/**
-		 * Returns a non-modifiable view of the specified row.
-		 *
-		 * @param i Row index
-		 */
-		CRowView operator[](const size_t i) const;
-
-		/// <summary>
-		/// operation to retrieve an element of an array that is mutable
-		/// </summary>
-		/// <param name="row"></param>
-		/// <param name="col"></param>
-		/// <returns></returns>
-		XlVariant& operator () (size_t row,  size_t col);
-
-		/// <summary>
-		/// operation to retrieve an element of an array that is inmutable
-		/// </summary>
-		/// <param name="row"></param>
-		/// <param name="col"></param>
-		/// <returns></returns>
-		const XlVariant& operator () (const size_t row, const size_t col) const;
-
-		// TODO: can add ref-qualified overload that moves values
-		std::vector<XlVariant> ToVector() const;
-
-		size_t rows() const { return m_rows_; };
-		size_t cols() const { return m_cols_; };
-
-	private:
-		size_t m_rows_;
-		size_t m_cols_;
-		std::vector<RowData> m_data_;
+			size_t m_rows_;
+			size_t m_cols_;
+			std::vector<std::vector<XlVariant>> m_data_;
 	};
+}
 
-}  // namespace oxl::xl_api
+
 
 #endif //OXL_XL_API_XL_ARRAY_H_
