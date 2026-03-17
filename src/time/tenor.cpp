f2424af40d60f2e5ca231e5648cbfc450205f0ec
Derek Huang <djh458@stern.nyu.edu>
2026-03-17 00:12:48 -0400

    enhance + clean up Tenor with more operators + FNV-1 hash function
    modified:   src/time/tenor.cpp
    modified:   src/time/tenor.h

--- src/time/tenor.cpp
+++ src/time/tenor.cpp
@@ -1,45 +1,166 @@
 #include "tenor.h"
 
-#include <boost/algorithm/string.hpp>
+#include <cctype>
+#include <cstddef>
+#include <ostream>
+#include <string>
 
 #include "helpers/utils.h"
 
-namespace oa
+namespace oa {
+namespace time {
+
+////////////////////////////////////////////////////////////////////////////////
+// Tenor::GroupLess                                                           //
+////////////////////////////////////////////////////////////////////////////////
+
+bool
+Tenor::GroupLess::operator()(const Tenor& a, const Tenor& b) const noexcept
 {
-	namespace time
+	// convert enum to ranking
+	// TODO: can have this as a free function later
+	auto rank = [](Tenors u) noexcept
 	{
-		Tenor::Tenor(const std::string& input_string)
-		{
-			try
-			{
-				if (oa::utils::CheckTenorStr(input_string))
-				{
-					m_number_ = std::stoi(input_string.substr(0, input_string.size() - 1));
-					m_time_unit_ = oa::utils::ValueToEnum<time::Tenors>(toupper(input_string.back()));
-				}
-				else
-				{
-					throw "Invalid tenor type please check your input of " + input_string;
-				}
-			}
-			catch (const std::exception&) {}
+		switch (u) {
+		case Tenors::kDays:
+			return 0u;
+		case Tenors::kWeeks:
+			return 1u;
+		case Tenors::kMonths:
+			return 2u;
+		case Tenors::kYears:
+			return 3u;
+		// bogus ranking value to ensure bad values are ordered last
+		default:
+			return 100u;
 		}
+	};
+	// get a and b rank
+	auto a_rank = rank(a.unit());
+	auto b_rank = rank(b.unit());
+	// if rank is the same, compare counts, otherwise compare ranks
+	return (a_rank == b_rank) ? (a.count() < b.count()) : (a_rank < b_rank);
+}
 
+////////////////////////////////////////////////////////////////////////////////
+// Tenor                                                                      //
+////////////////////////////////////////////////////////////////////////////////
 
-		Tenor::Tenor(int time_length, oa::time::Tenors tenor_enum) : m_number_(time_length), m_time_unit_(tenor_enum)
-		{
+// TODO: clean this function up later
+Tenor::Tenor(const std::string& input_string)
+{
+	if (oa::utils::CheckTenorStr(input_string))
+	{
+		m_number_ = std::stoi(input_string.substr(0, input_string.size() - 1));
+		m_time_unit_ = oa::utils::ValueToEnum<time::Tenors>(std::toupper(input_string.back()));
+	}
+	else
+		throw "Invalid tenor type please check your input of " + input_string;
+}
 
-		}
+Tenor::Tenor(int count, oa::time::Tenors unit) noexcept
+	: m_number_(count), m_time_unit_(unit)
+{}
 
-		const Tenor Tenor::FlipSign() const
-		{
-			return Tenor(std::to_string( -m_number_) +
-				static_cast<char> (m_time_unit_));
-		}
+Tenor Tenor::FlipSign() const
+{
+	// note: old implementation used the std::string ctor which is inefficient
+	return {-count(), unit()};
+}
 
-		const std::pair<int, oa::time::Tenors>Tenor::GetValues() const
-		{
-			return std::pair<int, oa::time::Tenors>(m_number_, m_time_unit_);
+std::pair<int, oa::time::Tenors>Tenor::GetValues() const
+{
+	return std::pair<int, oa::time::Tenors>(m_number_, m_time_unit_);
+}
+
+int
+Tenor::count() const noexcept
+{
+	return m_number_;
+}
+
+Tenors
+Tenor::unit() const noexcept
+{
+	return m_time_unit_;
+}
+
+Tenor
+Tenor::operator-() const noexcept
+{
+	return {-m_number_, m_time_unit_};
+}
+
+bool
+Tenor::operator==(const Tenor& other) const noexcept
+{
+	return (count() == other.count()) && (unit() == other.unit());
+}
+
+std::size_t
+Tenor::hash() const noexcept
+{
+	// FNV prime
+	constexpr auto prime = []() -> std::size_t
+	{
+		if constexpr (sizeof(std::size_t) == 4u)  // 32 bit
+			return 0x1000193;
+		else
+			return 0x100000001b3;                 // 64 bit
+	}();
+	// initial result (offset)
+	auto result = []() -> std::size_t
+	{
+		if constexpr (sizeof(std::size_t) == 4u)  // 32 bit
+			return 0x811c9dc5;
+		else
+			return 0xcbf29ce484222325;            // 64 bit
+	}();
+	// hash tenor count byte-by-byte
+	{
+		auto num = count();
+		// for each byte do multiply + XOR
+		for (auto i = 0u; i < sizeof(decltype(num)); i++) {
+			result *= prime;
+			result ^= (0xFF & num);
+			num >>= 8;
 		}
 	}
+	// tenor unit fits in one byte so only hash low byte
+	result *= prime;
+	result ^= (0xFF & static_cast<int>(unit()));
+	// done
+	return result;
 }
+
+Tenor
+Tenor::operator+(int shift) const noexcept
+{
+	return {count() + shift, unit()};
+}
+
+Tenor
+Tenor::operator-(int shift) const noexcept
+{
+	return {count() - shift, unit()};
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Tenor non-member operators                                                 //
+////////////////////////////////////////////////////////////////////////////////
+
+Tenor operator+(int shift, const Tenor& tenor) noexcept
+{
+	return tenor + shift;  // calls Tenor::operator+
+}
+
+std::ostream& operator<<(std::ostream& out, const Tenor& tenor)
+{
+	// convert enum value to lowercase (ASCII offset of 'a' - 'A' is 32)
+	auto unit = static_cast<char>(static_cast<int>(tenor.unit()) + 32);
+	// write
+	return out << tenor.count() << unit;
+}
+
+}  // namespace time
+}  // namespace oa
--- src/time/tenor.h
+++ src/time/tenor.h
@@ -1,38 +1,184 @@
-#ifndef OPENANALYTICS_TIME_TENOR_H
-#define OPENANALYTICS_TIME_TENOR_H
+#ifndef OA_TIME_TENOR_H_
+#define OA_TIME_TENOR_H_
 
+#include <cstddef>
+#include <iosfwd>
 #include <string>
 
 #include "oa/dllexport.h"
 #include "time_enums.h"
 
-//tc = tenor class
-namespace oa::time
-{
-	class OA_TIME_API Tenor
-	{
-		public:
-			Tenor(const std::string& input_string);
+namespace oa::time {
 
-			Tenor(int time_length, oa::time::Tenors tenor_enum);
-			/// <summary>
-			/// flips the sign of the tenor
-			/// </summary>
-			/// <returns>New Tenor object withteh sign flipped</returns>
-			const Tenor FlipSign() const;
-
-			/// <summary>
-			/// returns a pair that is the integer and the unit of time a char
-			/// </summary>
-			/// <returns>a pair where the first length and the second argument is lenght as a Enum</returns>
-			const std::pair<int, oa::time::Tenors> GetValues() const;
-
-		private:
-			int m_number_ = 0;
-			oa::time::Tenors m_time_unit_{};
+class OA_TIME_API Tenor {
+public:
+	/**
+	 * `Tenor` comparison functor that groups relation by time unit.
+	 *
+	 * This provides a strict weak ordering of `Tenor` objects based on their
+	 * time unit and then provides a strict ordering within each time unit
+	 * category based on the unit count. For example, any `Tenor` that is in
+	 * days is automatically ordered before a `Tenor` in weeks, even if the
+	 * number of days exceeds 7, e.g. `12d` is ordered before `1w`. Another
+	 * example would be `24m` being ordered before `1y`.
+	 *
+	 * Although this property makes this *Compare* functor inappropriate for
+	 * imposing a total ordering in an arithmetic sense, grouping tenors by
+	 * unit provides an ordering familiar to traders, and allows use with
+	 * `std::map` so a `Tenor` can be used as a key.
+	 */
+	struct OA_TIME_API GroupLess {
+		/**
+		 * Indicate if one `Tenor` is ordered before the other.
+		 *
+		 * Ordering is strictly grouped based on the length of the time unit
+		 * while intra-group ordering is strict based on count.
+		 *
+		 * @param a First tenor
+		 * @param b Second tenor
+		 */
+		bool operator()(const Tenor& a, const Tenor& b) const noexcept;
 	};
-}
 
+	/**
+	 * Ctor.
+	 *
+	 * Construct from a valid tenor string like `"3w"`. The case of the given
+	 * time unit can be lower or upper case, i.e. `"3W"` is also allowed.
+	 *
+	 * @todo Should use `string_view` to avoid needing to create a string.
+	 *
+	 * @param str Tenor string, e.g. `"2y"` or `"2Y"`
+	 */
+	Tenor(const std::string& str);
 
+	/**
+	 * Ctor.
+	 *
+	 * Construct the tenor from a time unit and the number of time units.
+	 *
+	 * @param count Number of time units
+	 * @param unit Tenor time unit enum value
+	 */
+	Tenor(int count, Tenors unit) noexcept;
 
-#endif // !OPENANALYTICS_TIME_TENOR_H
+	// TODO: deprecate in factor of unary negation
+	/// <summary>
+	/// flips the sign of the tenor
+	/// </summary>
+	/// <returns>New Tenor object withteh sign flipped</returns>
+	Tenor FlipSign() const;
+
+	// TODO: deprecate in factor of just using count() and unit()
+	/// <summary>
+	/// returns a pair that is the integer and the unit of time a char
+	/// </summary>
+	/// <returns>a pair where the first length and the second argument is lenght as a Enum</returns>
+	std::pair<int, oa::time::Tenors> GetValues() const;
+
+	/**
+	 * Return the number of units represented by the tenor.
+	 */
+	int count() const noexcept;
+
+	/**
+	 * Return the time unit value.
+	 */
+	Tenors unit() const noexcept;
+
+	/**
+	 * Flip the sign of the tenor.
+	 *
+	 * This produces a new `Tenor` object of same unit with negated count.
+	 */
+	Tenor operator-() const noexcept;
+
+	/**
+	 * Compare against another tenor for equality.
+	 *
+	 * Tenor equality is done strictly in terms of the count + unit together.
+	 * Therefore, a tenor of 7d is *not* considered equal to 1w.
+	 *
+	 * @param other Tenor to compare against
+	 */
+	bool operator==(const Tenor& other) const noexcept;
+
+	/**
+	 * Return a hash value for the `Tenor`.
+	 *
+	 * The hash value is computed for 32- and 64-bit platforms using the
+	 * FNV-1 hash algorithm. In theory, however, if we restricted the value
+	 * of count to fit within 3 bytes, then we could use this implementation:
+	 *
+	 * @code{.cc}
+	 * (std::size_t{count() } << 8) | (0xFF & static_cast<int>(unit()))
+	 * @endcode
+	 *
+	 * This takes advantage of the fact that the values of the `Tenors` enum
+	 * all nicely fit within a single byte so we only need the low byte.
+	 */
+	std::size_t hash() const noexcept;
+
+	/**
+	 * Add the given count to create a new `Tenor` object.
+	 *
+	 * @param shift Unit quantity to add
+	 */
+	Tenor operator+(int shift) const noexcept;
+
+	/**
+	 * Subtract the given count to create a new `Tenor` object.
+	 *
+	 * @note There is no accompanying operator for `int` - `Tenor`.
+	 *
+	 * @param shift Unit quantity to subtract
+	 */
+	Tenor operator-(int shift) const noexcept;
+
+private:
+	// TODO: rename members so they are semantically consistent with getters
+	int m_number_;
+	Tenors m_time_unit_;
+};
+
+/**
+ * Add the given count to create a new `Tenor` object.
+ *
+ * @param shift Unit quantity to add
+ * @param tenor Tenor to adjust
+ */
+OA_TIME_API
+Tenor operator+(int shift, const Tenor& tenor) noexcept;
+
+/**
+ * Write the `Tenor` to the output stream.
+ *
+ * This will write the natural language form of a tenor, e.g. `"3m"`.
+ *
+ * @param out Output stream
+ * @param tenor Tenor to write
+ */
+OA_TIME_API
+std::ostream& operator<<(std::ostream& out, const Tenor& tenor);
+
+}  // namespace oa::time
+
+namespace std {
+
+/**
+ * `std::hash` specialization for the `Tenor`.
+ */
+template <>
+struct hash<oa::time::Tenor> {
+	/**
+	 * Return the value of `Tenor::hash()` on the given `Tenor`.
+	 */
+	auto operator()(const oa::time::Tenor& tenor) const noexcept
+	{
+		return tenor.hash();
+	}
+};
+
+}  // namespace std
+
+#endif  // OA_TIME_TENOR_H_
