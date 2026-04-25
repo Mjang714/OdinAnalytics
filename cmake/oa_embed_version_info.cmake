e3fb464d7d196a9d025a3463e3aeeb9b35b411db
Derek Huang <djh458@stern.nyu.edu>
2026-04-25 14:35:49 -0400

    use custom command instead of configure_file for RC version compile
    modified:   cmake/oa_embed_version_info.cmake
    renamed:    cmake/version.rc.in -> src/version.rc

--- cmake/oa_embed_version_info.cmake
+++ cmake/oa_embed_version_info.cmake
@@ -14,9 +14,10 @@ include_guard(GLOBAL)
 ##
 # Embed version information into the target binary.
 #
-# On Windows this embeds the resource info compiled from version.rc.in and
-# appropriately defines any macros needed for the resource compiler invocation.
-# The version.rc file is also appropriately included as a target source.
+# On Windows this embeds the resource info compiled from version.rc into a .res
+# resource file for the target and appropriately defines any macros needed for
+# the resource compiler invocation. The version.rc file is also appropriately
+# included as a target source and as a LINK.EXE input file.
 #
 # Currently this function is a no-op on Linux. We may consider an alternate
 # embedding scheme involving adding sections to an ELF object.
@@ -58,43 +59,52 @@ function(oa_embed_version_info target)
     else()
         message(FATAL_ERROR "cannot use ${target_type} target with version.rc")
     endif()
-    # set variables for configure_file() call on version.rc.in
-    set(OA_TARGET "${target}")
-    set(OA_RC_FILE_TYPE "${rc_file_type}")
-    set(OA_RC_FILE_DESCRIPTION "${ARG_DESCRIPTION}")
-    # current binary subdirectory
-    cmake_path(
-        RELATIVE_PATH CMAKE_CURRENT_BINARY_DIR
-        BASE_DIRECTORY "${PROJECT_BINARY_DIR}"
-        OUTPUT_VARIABLE out_subdir
+    # indicate if multi-config generator + set .res file output directory
+    get_property(is_multi GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+    set(res_outdir_genex "${CMAKE_CURRENT_BINARY_DIR}")
+    if(is_multi)
+        string(APPEND res_outdir_gene "/$<CONFIG>")
+    endif()
+    # target-specific resource file name
+    set(res_outname "${target}_version.res")
+    # custom command for .res file invoking CMAKE_RC_COMPILER
+    add_custom_command(
+        OUTPUT "${res_outdir_genex}/${res_outname}"
+        COMMAND "${CMAKE_RC_COMPILER}"
+                /nologo
+                # repeat some standard Win32 compiler defines
+                /DWIN32 /D_WIN32 /D_WINDOWS
+                # define _DEBUG like MSVC if building Debug config
+                $<$<CONFIG:Debug>:/D_DEBUG>
+                # pass file type, file name, description through macro
+                /DOA_RC_FILE_TYPE=${rc_file_type}
+                # note: RC has some interesting escaping requirements. for
+                # example, the quoting needs to be done *after* the option, so
+                # for a quoted string specified with /D, we have to do:
+                #
+                #   /D"MY_MACRO=\"this is my string\""
+                #
+                # so in CMake land, we have the escape the \ as well. this
+                # pattern is used for any quoted option argument.
+                "/D\"OA_RC_FILE_NAME=\\\"$<TARGET_FILE_NAME:${target}>\\\"\""
+                "/D\"OA_RC_FILE_DESCRIPTION=\\\"${ARG_DESCRIPTION}\\\"\""
+                # default language is US English
+                /l0x0409
+                # ensure version.h is on include path
+                "/I\"${PROJECT_BINARY_DIR}/include\""
+                # output + input
+                "/fo\"${res_outdir_genex}/${res_outname}\""
+                "\"${PROJECT_SOURCE_DIR}/src/version.rc\""
+        # hardcoded dependencies as RC doesn't do dependency scanning
+        DEPENDS "${PROJECT_BINARY_DIR}/include/oa/version.h"
+                "${PROJECT_SOURCE_DIR}/src/version.rc"
+        COMMENT "RC compile version.rc into ${res_outname}"
+        # note: don't use VERBATIM since RC has some weird behavior when it
+        # comes to escaping arguments. see the above comment
     )
-    # configure version.rc.in -> ${target}_version.rc
-    #
-    # variables consumed:
-    #
-    #   OA_TARGET
-    #   OA_RC_FILE_TYPE
-    #   OA_RC_FILE_DESCRIPTION
-    #
-    configure_file(
-        "${PROJECT_SOURCE_DIR}/cmake/version.rc.in" ${target}_version.rc
-        @ONLY NEWLINE_STYLE LF
-    )
-    message(STATUS "Generated ${out_subdir}/${target}_version.rc")
-    # add RC file to target sources + ensure generated oa/version.h included
-    target_sources(
-        ${target} PRIVATE
-        "${CMAKE_CURRENT_BINARY_DIR}/${target}_version.rc"
-    )
-    target_include_directories(${target} PRIVATE "${PROJECT_BINARY_DIR}/include")
-    # compile with OA_RC_FILE_NAME defined for per-config file name
-    # TODO: we can use generator expressions in compile definitions so maybe we
-    # don't need to have any custom RC invocation rules. we may also not even
-    # need a configure_file() step and could define more macros. however, the
-    # downside is that OA_RC_FILE_NAME is also defined for C/C++ compilation,
-    # which strictly speaking, is not something we want to happen
-    target_compile_definitions(
-        ${target} PRIVATE
-        OA_RC_FILE_NAME="$<TARGET_FILE_NAME:${target}>"  # quotes preserved
-    )
+    # add .res resource file to list of target sources to trigger compile rule
+    # but also add to linker inputs to force LINK.EXE to process the .res file
+    # as by default, only .obj files are processed as linker input
+    target_sources(${target} PRIVATE "${res_outdir_genex}/${res_outname}")
+    target_link_libraries(${target} PRIVATE "${res_outdir_genex}/${res_outname}")
 endfunction()
