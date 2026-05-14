f286815d1c8f35680d489686c31c3e496e513646
Derek Huang <djh458@stern.nyu.edu>
2026-05-13 23:26:00 -0400

    time_enum_mappers.h: clean up decls + ensure all content is guarded

--- src/enum_mappers/time_enum_mappers.h
+++ src/enum_mappers/time_enum_mappers.h
@@ -1,17 +1,19 @@
+// time_enum_mappers.h
+
+#ifndef OA_ENUM_MAPPERS_TIME_ENUM_MAPPERS_H_
+#define OA_ENUM_MAPPERS_TIME_ENUM_MAPPERS_H_
+
+#include <iosfwd>
 #include <string>
-#include <ostream>
 
 #include "time/time_enums.h"
 #include "derived_time/derived_time_enums.h"
 
 #include "oa/dllexport.h"
 
-#ifndef OA_ENUM_MAPPERS_TIME_ENUM_MAPPERS_H_
-#define OA_ENUM_MAPPERS_TIME_ENUM_MAPPERS_H_
+namespace oa {
 
-namespace oa::enum_mappers {
-
-
+namespace enum_mappers {
 
 /// <summary>
 /// returns an DayCountRule enum from a given string
@@ -36,32 +38,36 @@ derived_time::StubType MapInputToStub(const std::string& input_str);
 OA_ENUM_MAPPERS_API
 derived_time::CalcType MapInputToCalcType(const std::string& input_str);
 
-}  // namespace oa::enum_mappers
+}  // namespace enum_mappers
 
+// TODO: consider declaring operator<< near related enums instead
 
-namespace oa::derived_time {
-
-    OA_ENUM_MAPPERS_API
-    std::ostream& operator<<(std::ostream& os, DateDirection val);
-
-    OA_ENUM_MAPPERS_API
-    std::ostream& operator<<(std::ostream& os, Frequency val);
-
-    OA_ENUM_MAPPERS_API
-    std::ostream& operator<<(std::ostream& os, StubType val);
-
-    OA_ENUM_MAPPERS_API
-    std::ostream& operator<<(std::ostream& os, CalcType val);
-}
-
-//not sure if this makes sense to keep there was thinking of moving it time_enum.h bu then we would ened to add a cpp file 
-namespace oa::time {
+namespace derived_time {
 
 OA_ENUM_MAPPERS_API
-std::ostream& operator <<(std::ostream& os, DayCountRule val);
+std::ostream& operator<<(std::ostream& os, DateDirection val);
 
 OA_ENUM_MAPPERS_API
-std::ostream& operator <<(std::ostream& os, AdjRule val);
+std::ostream& operator<<(std::ostream& os, Frequency val);
 
-}  // namespace oa::time
+OA_ENUM_MAPPERS_API
+std::ostream& operator<<(std::ostream& os, StubType val);
+
+OA_ENUM_MAPPERS_API
+std::ostream& operator<<(std::ostream& os, CalcType val);
+
+}  // namespace derived_time
+
+namespace time {
+
+OA_ENUM_MAPPERS_API
+std::ostream& operator<<(std::ostream& os, DayCountRule val);
+
+OA_ENUM_MAPPERS_API
+std::ostream& operator<<(std::ostream& os, AdjRule val);
+
+}  // namespace time
+
+}  // namespace oa
+
 #endif // !OA_ENUM_MAPPERS_TIME_ENUM_MAPPERS_H_
