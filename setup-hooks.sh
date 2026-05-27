0b516f63f5bb712dd84c6adb711007eab7c30ae1
Derek Huang <djh458@stern.nyu.edu>
2026-05-27 02:00:23 -0400

    add pre-commit hook for checking file names + trailing blanks + setup script
    new file:   setup-hooks.sh
    new file:   tools/git/pre-commit

---
+++ setup-hooks.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/sh
+#
+# File: setup-hooks.sh
+# Author: Derek Huang
+# Copyright: MIT License
+#
+# Git hooks setup script.
+#
+# This simply copies all the hook scripts in this directory to .git/hooks and
+# marks the copies as executable with chmod +x.
+#
+
+# must run where .git directory is
+if ! [ -d .git ]
+then
+    echo "error: must be run from repo root, i.e. where the .git directory is"
+    exit 1
+fi
+
+# copy all hook scripts and ensure they are executable
+for hook in $(ls tools/git)
+do
+    # copy + ensure executable
+    cp tools/git/$hook .git/hooks && chmod +x .git/hooks/$hook
+    # verify that hook is installed and executable
+    if ! [ -x .git/hooks/$hook ]
+    then
+        echo "error: failed to install git $hook hook"
+        exit 1
+    fi
+done
