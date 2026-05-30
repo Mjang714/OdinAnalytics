54bc1ca54110a49facbac13633a9b1af31cd4ba4
Derek Huang <djh458@stern.nyu.edu>
2026-05-29 21:06:40 -0400

    time_enum_mappers.h: indentation + header reformat

--- src/time/time_enum_mappers.h
+++ src/time/time_enum_mappers.h
@@ -1,18 +1,19 @@
 #ifndef ODINANALYTICS_TIME_TIME_ENUM_MAPPERS_H_
 #define ODINANALYTICS_TIME_TIME_ENUM_MAPPERS_H_
 
+#include <iostream>
+#include <string>
+
 #include "oa/dllexport.h"
 #include "time/time_enums.h"
 
-#include <string>
-#include <iostream>
-
 namespace oa::time {
-    OA_TIME_API
-    oa::time::DayCountRule MapInputToDayCountEnum(const std::string& input_str);
 
-    OA_TIME_API
-    oa::time::AdjRule MapInputToDayAdjustEnum(const std::string& input_str);
+OA_TIME_API
+oa::time::DayCountRule MapInputToDayCountEnum(const std::string& input_str);
+
+OA_TIME_API
+oa::time::AdjRule MapInputToDayAdjustEnum(const std::string& input_str);
 
 }  // namespace oa::time
 
