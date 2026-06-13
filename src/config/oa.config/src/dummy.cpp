486c09bbc9156ac8c8561a9996023abbde2e83dd
Derek Huang <djh458@stern.nyu.edu>
2026-06-13 10:28:33 -0400

    add basic R package wrapping oa_config as a shim over SWIG
    new file:   src/config/oa.config/DESCRIPTION
    new file:   src/config/oa.config/LICENSE
    new file:   src/config/oa.config/src/Makevars.in
    new file:   src/config/oa.config/src/dummy.cpp

---
+++ src/config/oa.config/src/dummy.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file dummy.cpp
+ * @author Derek Huang
+ * @brief C++ dummy for R CMD INSTALL
+ * @copyright MIT License
+ *
+ * This file exists just to provide something for `R CMD INSTALL` to compile.
+ * We will rely on the `configure` script to provide the actual object files
+ * and libraries that are required for linking.
+ */
+
+#ifndef __cplusplus
+#error "dummy.cpp: must use C++ compiler"
+#endif  // __cplusplus)
