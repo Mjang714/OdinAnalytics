04245a21991bca8058625e4e560bfe6b73806d5b
Derek Huang <djh458@stern.nyu.edu>
2026-06-14 01:03:44 -0400

    add oa_r_discover_tests() for R CTest integration + add more VersionInfo tests
    modified:   CMakeLists.txt
    new file:   cmake/oa_r_discover_tests.cmake
    modified:   test/CMakeLists.txt
    modified:   test/oa_config_test.R

--- test/oa_config_test.R
+++ test/oa_config_test.R
@@ -14,16 +14,12 @@ cvi <- VersionInfo(library_path())
 
 # test that library_dir() and data_dir() are the same
 test_library_data_dirs <- function() {
-  # note: SWIG adds an extra argument that we can ignore
-  lib_dir <- library_dir()
-  dat_dir <- data_dir()
-  # check
-  if (lib_dir == dat_dir) {
-    return(TRUE)
-  } else {
-    message("library_dir() != dat_dir(): [", lib_dir, " != ", dat_dir, "]")
-    return(FALSE)
-  }
+  oa.tu_expect_eq(library_dir(), data_dir())
+}
+
+# test that VersionInfo$company() is the expected "OA Developers" string
+test_cvi_company <- function() {
+  oa.tu_expect_eq("OA Developers", cvi$company())
 }
 
 # test that VersionInfo$filename() is the file name from library_path()
@@ -31,37 +27,35 @@ test_cvi_filename <- function() {
   # path to active oa_config library
   path <- library_path()
   # oa_config filename from library_path() substring + from VersionInfo
-  name_1 <- substring(path, regexpr("[/\\][^/\\]+$", path) + 1)
-  name_2 <- cvi$filename()
-  # check
-  if (name_1 == name_2) {
-    return(TRUE)
-  } else {
-    message(
-      "substring(path, regexpr(\"[/\\][^/\\]+$\", path) + 1) != ",
-      "cvi$filename(): [", name_1, " != ", name_2, "]"
-    )
-    return(FALSE)
-  }
+  oa.tu_expect_eq(
+    substring(path, regexpr("[/\\][^/\\]+$", path) + 1),
+    cvi$filename()
+  )
 }
 
-# test that VersionInfo$company() is the expected "OA Developers" string
-test_cvi_company <- function() {
-  s1 <- "OA Developers"
-  s2 <- cvi$company()
-  if (s1 == s2) {
-    return(TRUE)
-  } else {
-    message("\"OA Developers\" != cvi$company(): [", s1, " != ", s2, "]")
-    return(FALSE)
-  }
+# test that VersionInfo$product() is the expected "OA Software Suite" string
+test_cvi_product <- function() {
+  oa.tu_expect_eq("OA Software Suite", cvi$product())
+}
+
+# test that VersionInfo$version_num() components are as expected
+test_cvi_version_num <- function() {
+  # SWIG wrapper produced an S4 class type since R only has 32-bit signed int
+  vn <- cvi$version_num()
+  # check each component is nonnegative
+  oa.tu_expect_ge(vn$w1(), 0) &&
+  oa.tu_expect_ge(vn$w2(), 0) &&
+  oa.tu_expect_ge(vn$w3(), 0) &&
+  oa.tu_expect_ge(vn$w4(), 0)
 }
 
 # named vector of tests to run
 tests = c(
   test_library_data_dirs = test_library_data_dirs,
   test_cvi_filename = test_cvi_filename,
-  test_cvi_company = test_cvi_company
+  test_cvi_company = test_cvi_company,
+  test_cvi_product = test_cvi_product,
+  test_cvi_version_num = test_cvi_version_num
 )
 
 # main
