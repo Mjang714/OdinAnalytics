a519a3c2a8d53fd1005fd71b2ecf42567376fd4e
Mjang714 <mjang714@gmail.com>
2026-07-04 17:53:42 -0400

    Finished adding unit test for string Date constuctor

--- src/unit_test/date_test.cpp
+++ src/unit_test/date_test.cpp
@@ -10,6 +10,9 @@ namespace
 		EXPECT_ANY_THROW(oa::time::Date("19-10-2004"));
 		EXPECT_ANY_THROW(oa::time::Date("12-10-2007"));
 		EXPECT_ANY_THROW(oa::time::Date("12-10-2OO7"));
+		EXPECT_ANY_THROW(oa::time::Date("12-00-2007"));
+		EXPECT_ANY_THROW(oa::time::Date("12-32-2007"));
+		EXPECT_ANY_THROW(oa::time::Date("2-30-2008"));
 	}
 
 	TEST(DateClassTest, ValidContructor)
@@ -17,6 +20,8 @@ namespace
 		EXPECT_NO_THROW(oa::time::Date("1900-12-04"));
 		EXPECT_NO_THROW(oa::time::Date("1900:12:04"));
 		EXPECT_NO_THROW(oa::time::Date("1900/12/04"));
+		EXPECT_NO_THROW(oa::time::Date("12-31-2007"));
+		EXPECT_NO_THROW(oa::time::Date("2-28-2008"));
 
 	}
 
